6877748a0d81fe63a1dbf73d14f837ee2f87c1c3
Jens <jens.weber@ka-raceing.de>
2017-11-21 17:33:13 +0100

    edited A1-A3; added matlab.pdf as intro in Uebung1

--- Uebung6_Abgabe/MathPendel_Bearbeitungsfile.m
+++ Uebung6_Abgabe/MathPendel_Bearbeitungsfile.m
@@ -4,54 +4,56 @@ close all
 clc
 
 %% Parameter
-g = 'ausfüllen';   %% Erdbeschleunigung
-l = 'ausfüllen';   %% Länge l
-AnzahlSchritte = 'ausfüllen';      %% Anzahl der Zeitschritte
-h = 'ausfüllen';                  %% Zeitschrittweite
-x_0 = 'ausfüllen';            %% Anfangsbedingungen in Zustandsvektor
-t(1)='ausfüllen';                     %% Initialisierung der Zeit
+g = 9.81;                   % Erdbeschleunigung (m/s^2)
+l = .2;                     % Laenge l (m)
+AnzahlSchritte = 10e4;      % Anzahl der Zeitschritte
+h = .001;                   % Zeitschrittweite
+x_0 = [pi/6;0];             % Anfangsbedingungen in Zustandsvektor; x_0=[phi_0; phiDot_0]
+t(1)=0;                     % Initialisierung der Zeit
 
 
-x_Eu_expl(:,1) = 'ausfüllen';       %% Anfangsbedingungen für Euler explizit
-x_Eu_impl(:,1) = 'ausfüllen';       %% Anfangsbedingungen für Euler implizit
-x_RuKu(:,1) = 'ausfüllen';          %% Anfangsbedingungen für Runge-Kutta-Verfahren
+x_Eu_expl(:,1) = x_0;       % Anfangsbedingungen fuer Euler explizit
+x_Eu_impl(:,1) = x_0;       % Anfangsbedingungen fuer Euler implizit
+x_RuKu(:,1) = x_0;          % Anfangsbedingungen fuer Runge-Kutta-Verfahren
 
-SystMatr='ausfüllen';          %% Systemmatrix
-  
+SystMatr = [0 1 ; -g/l 0];  % Systemmatrix
+    %% Aufgabe 2: Analytische Loesung der linearisierten DGL:
+y=dsolve('D2phi + g/l*phi = 0', 'phi(0)=pi/6', 'Dphi(0)=0')
+yValues=dsolve('D2phi + 9.81/0.2*phi = 0', 'phi(0)=pi/6', 'Dphi(0)=0')
 
 
-for n = 'ausfüllen'
+for n = AnzahlSchritte
     %% Aufgabe 3: Euler explizit
-    x_Eu_expl(:,n+1)='ausfüllen';
+    x_Eu_expl(:,n+1) = x_Eu_expl(n) + SystMatr*(SystMatr*x_Eu_expl(n)*h)*h;    % In x_Eu_expl wird dynamisch immer ein Spaltenvektor [phi; phiDot] angefuegt
        
     %% Aufgabe 4: Euler implizit
-    x_Eu_impl(:,n+1) ='ausfüllen';
+    x_Eu_impl(:,n+1) ='ausfï¿½llen';
     
     %% Aufgabe 5: Runge-Kutta-Verfahren    
-    k1(:,n)='ausfüllen';
-    k2(:,n)='ausfüllen';
-    k3(:,n)='ausfüllen';
-    k4(:,n)='ausfüllen';
-    x_RuKu(:,n+1) = 'ausfüllen';
+    k1(:,n)='ausfï¿½llen';
+    k2(:,n)='ausfï¿½llen';
+    k3(:,n)='ausfï¿½llen';
+    k4(:,n)='ausfï¿½llen';
+    x_RuKu(:,n+1) = 'ausfï¿½llen';
     
-    t(n+1)='ausfüllen';
+    t(n+1)='ausfï¿½llen';
 end
 
-%% Aufgabe 2: exakte Lösung
-x_exakt = 'ausfüllen';
+%% Aufgabe 2: exakte Lï¿½sung
+x_exakt = 'ausfï¿½llen';
 
 %% Aufgabe 6
 
-l_Eu_expl = 'ausfüllen';
-l_Eu_impl = 'ausfüllen';
-l_RuKu = 'ausfüllen';
+l_Eu_expl = 'ausfï¿½llen';
+l_Eu_impl = 'ausfï¿½llen';
+l_RuKu = 'ausfï¿½llen';
 
-e_Eu_expl = 'ausfüllen';
-e_Eu_impl = 'ausfüllen';
-e_RuKu = 'ausfüllen';
+e_Eu_expl = 'ausfï¿½llen';
+e_Eu_impl = 'ausfï¿½llen';
+e_RuKu = 'ausfï¿½llen';
 
 %% Aufgabe 7: Plots
-'ausfüllen';
+'ausfï¿½llen';
 
 %% Aufgabe 8: Plots
-'ausfüllen';
+'ausfï¿½llen';
